2b3829023abcca9f9401c5fe97aba2ed08760166
Sahil <gsahil307@gmail.com>
2024-06-30 04:51:14 +0530

    Add Recipe Model
    Model is like a class in c

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://i.ytimg.com/vi/pdFm9Rnz4H8/maxresdefault.jpg')
+  ];
 
 }
